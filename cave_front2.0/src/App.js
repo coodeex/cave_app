ed68cc8714cf32b7e93b65da1a9af5e43a4e889c
coodeex <juhani.s.lehto@gmail.com>
2021-02-15 22:54:10 +0200

    refactor ProductList

--- cave_front2.0/src/App.js
+++ cave_front2.0/src/App.js
@@ -1,9 +1,9 @@
 import React, { useState, useEffect } from 'react'
-import Product from './components/Product'
 import ProductForm from './components/ProductForm'
 import productService from './services/products'
 import LoginForm from './components/LoginForm'
 import Logout from './components/Logout'
+import ProductsList from './components/ProductsList'
 
 const App = () => {
   const [products, setProducts] = useState([])  // all products
@@ -34,16 +34,7 @@ const App = () => {
   return (
     <div>
       <h1>Products</h1>
-      <ul>
-        {products.slice().reverse().filter((v, i, a) => a.findIndex(t => (t.id === v.id)) === i).reverse().sort(function (a, b) {
-          if (a.name < b.name) { return -1; }
-          if (a.name > b.name) { return 1; }
-          return 0;
-        }).map((product, i) =>
-          <Product key={i} product={product} />
-        )}
-      </ul>
-
+      <ProductsList products={products}/>
       {user === null
         ? <LoginForm username={username} setUser={setUser} setUsername={setUsername} password={password} setPassword={setPassword}/>
         : <ProductForm products={products} setProducts={setProducts} newProductName={newProductName} setNewProductName={setNewProductName} newProductPrice={newProductPrice} setNewProductPrice={setNewProductPrice} newProductWeight={newProductWeight} setNewProductWeight={setNewProductWeight} />
